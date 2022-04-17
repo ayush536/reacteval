eef0516d105acc007cf71b317f1628f43089e9c0
ayush536 <82461982+ayush536@users.noreply.github.com>
2022-04-17 10:55:35 +0530

    final

--- src/cityCountry/action.jsx
+++ src/cityCountry/action.jsx
@@ -1,20 +1,13 @@
 import axios from "axios"
-
 import * as types from "./type"
-
 const countryAddReq = (payload) => (payload) => ({ type: types.COUNTRY_ADD_REQ, payload })
 const countryAdded = (payload) => (payload) => ({ type: types.COUNTRY_ADDED, payload })
 const countryAddFail = (payload) => (payload) => ({ type: types.COUNTRY_ADD_FAIL, payload })
-
 const cityAddReq = (payload) => (payload) => ({ type: types.CITY_ADD_REQ, payload })
 const cityAdded = (payload) => (payload) => ({ type: types.CITY_ADDED, payload })
 const cityAddFail = (payload) => (payload) => ({ type: types.CITY_ADD_FAIL, payload })
-
-
 const AddCountry = (country) => async (dispatch) => {
-
     dispatch(countryAddReq({ msg: "Loading" }))
-
     const res = await axios.post("http://localhost:8080/add-country", {
         country
     }).catch((err) => {
@@ -43,7 +36,5 @@ const AddCity = (city) => async (dispatch) => {
     console.log(res, "res")
 
 }
-
-
 
 export { AddCountry, AddCity }
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,12 +4,12 @@ import Table from '@mui/material/Table';
 export const HomePage = () => {
 
     return <>
-        <h1>Country-City Data</h1>
-        <Link to="/addcountry"> <Button className="button" variant="contained" color="success">
+        <h1>Country-City all kind of Data</h1>
+        <Link to="/addcountry"> <Button className="button" variant="contained" color="gray">
             Add Country
         </Button></Link>
         <Link to="/addcity">
-            <Button className="button" variant="contained" color="success">
+            <Button className="button" variant="contained" color="gray">
                 Add CIty
             </Button>
         </Link>
